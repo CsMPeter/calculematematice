59ec66b7e052a31bdae15a8f0a3953090016b3fc
CsMPeter <csurkampeter@gmail.com>
2019-09-09 18:30:26 +0300

    Calcule matematice

---
+++ src/main/java/org/fasttrackit/App.java
@@ -0,0 +1,18 @@
+package org.fasttrackit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Calcul calcul = new Calcul();
+
+        System.out.println(calcul.adunare(3,2));
+        System.out.println(calcul.inmultire(3,2));
+        System.out.println(calcul.scadere(3,2));
+        System.out.println(calcul.despartire(3,2));
+    }
+}
---
+++ src/main/java/org/fasttrackit/Calcul.java
@@ -0,0 +1,26 @@
+package org.fasttrackit;
+
+public class Calcul {
+
+
+
+    public int adunare(int x, int y){
+
+        return x+y;
+    }
+
+    public int inmultire(int x, int y){
+
+        return x*y;
+    }
+
+    public int despartire(int x, int y){
+
+        return x/y;
+    }
+
+    public int scadere(int x, int y){
+
+        return x-y;
+    }
+}
